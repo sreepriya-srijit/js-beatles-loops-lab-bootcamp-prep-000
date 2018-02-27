1dc1feede3bcc45dbefa226dbb1b160f8aa2b8dc
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-02-27 22:42:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newarray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newarray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newarray;
+}
+
+function johnLennonFacts(facts){
+  var len = 0;
+  var strarray = [];
+  while(len < facts.length)
+  {
+    strarray.push(facts[len] + " !!!")
+    len++;
+  }
+  return strarray;
+}
